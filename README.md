03cb9330bb43edbb0e19a20d8ebf88d0870095fb
hussein1997323 <husseinothman660@gmail.com>
2024-02-08 16:31:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# teste -cypress 
+server Id: kla2r2am
+key: vacJlr1wJp0xBRtctQ8G1ZJTGDuN7LgN
---
+++ cypress/support/e2e.js
@@ -0,0 +1,5 @@
+import 'cypress-mailosaur'
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
